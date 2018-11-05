7e46af68b98362036f01707db535e91934fa258c
fukuo-319 <fukkunn319@gmail.com>
2018-11-05 17:52:42 +0900

    1105

---
+++ src/com/internousdev/casablanca/action/ResetPasswordAction.java
@@ -0,0 +1,10 @@
+package com.internousdev.casablanca.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ResetPasswordAction extends ActionSupport {
+
+	public String execute(){
+		return SUCCESS;
+	}
+}
