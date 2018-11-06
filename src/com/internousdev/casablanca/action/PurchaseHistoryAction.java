91d1c5c4ea0d8160f01be95f5dd2bedd2ce14d78
yuzukabachika <yuzuka33m@yahoo.co.jp>
2018-11-06 14:36:03 +0900

    PurchaseHistoryAction!!!!!!

--- src/com/internousdev/casablanca/action/PurchaseHistoryAction.java
+++ src/com/internousdev/casablanca/action/PurchaseHistoryAction.java
@@ -1,56 +1,47 @@
 package com.internousdev.casablanca.action;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.casablanca.dao.MCategoryDAO;
-import com.internousdev.casablanca.dao.UserInfoDAO;
+import com.internousdev.casablanca.dao.PurchaseHistoryInfoDAO;
 import com.internousdev.casablanca.dto.MCategoryDTO;
-import com.internousdev.casablanca.dto.UserInfoDTO;
+import com.internousdev.casablanca.dto.PurchaseHistoryInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 
-public class MyPageAction extends ActionSupport implements SessionAware{
+public class PurchaseHistoryAction extends ActionSupport implements SessionAware{
 	private String categoryId;
 	private String keywords;
-	private List<MCategoryDTO> mCategoryDTOList=new ArrayList<MCategoryDTO>();
 
+	private List<MCategoryDTO> mCategoryDTOList=new ArrayList<MCategoryDTO>();
 	private Map<String,Object> session;
 
 	public String execute() {
-		String result=ERROR;
 		System.out.println(categoryId);
 		System.out.println(keywords);
-		UserInfoDAO userInfoDAO= new UserInfoDAO();
-		UserInfoDTO userInfoDTO=new UserInfoDTO();
-		userInfoDTO=userInfoDAO.getUserInfo(String.valueOf(session.get("loginId")));
-		session.put("familyName", userInfoDTO.getFamilyName());
-		session.put("firstName", userInfoDTO.getFirstName());
-		session.put("familyNameKana", userInfoDTO.getFamilyNameKana());
-		session.put("firstNameKana",userInfoDTO.getFirstNameKana());
-		session.put("sex", userInfoDTO.getSex());
-		session.put("email",userInfoDTO.getFirstNameKana());
-		System.out.println(session.get("familyName"));
-		result =SUCCESS;
 
+		PurchaseHistoryInfoDAO purchaseHistoryInfoDAO=new PurchaseHistoryInfoDAO();
+		List<PurchaseHistoryInfoDTO> purchaseHistoryInfoDTOList= new ArrayList<PurchaseHistoryInfoDTO>();
+		purchaseHistoryInfoDTOList=purchaseHistoryInfoDAO.getPurchaseHistoryList(String.valueOf(session.get("loginId")));
+		Iterator<PurchaseHistoryInfoDTO> iterator=purchaseHistoryInfoDTOList.iterator();
+		if(!(iterator.hasNext())) {
+			purchaseHistoryInfoDTOList=null;
+		}
+		session.put("purchaseHistoryInfoDTOList", purchaseHistoryInfoDTOList);
 
-	return result;
-
-
-
-
-	if(!session.containsKey("mCategoryList")) {
-		MCategoryDAO mCategoryDAO=new MCategoryDAO();
-		mCategoryDTOList= mCategoryDAO.getMCategoryList();
-		session.put("mCategoryDTOList", mCategoryDTOList);
-	}
-
+		if(!session.containsKey("mCategoryList")){
+			MCategoryDAO mCategoryDAO=new MCategoryDAO();
+			mCategoryDTOList= mCategoryDAO.getMCategoryList();
+			session.put("mCategoryDTOList", mCategoryDTOList);
+		}
+		return SUCCESS;
 
 	}
-
 
 	public String getCategoryId() {
 		return categoryId;
@@ -83,5 +74,8 @@ public class MyPageAction extends ActionSupport implements SessionAware{
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-}
 
+
+
+
+}
