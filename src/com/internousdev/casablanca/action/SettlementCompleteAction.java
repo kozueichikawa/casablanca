8e27e7d08d048c8a7bd0af7b6a3af6eaebc99009
kozueichikawa <wmwmwm1245@gmail.com>
2018-11-07 17:59:26 +0900

    コミコミ

--- src/com/internousdev/casablanca/action/SettlementCompleteAction.java
+++ src/com/internousdev/casablanca/action/SettlementCompleteAction.java
@@ -1,5 +1,15 @@
 package com.internousdev.casablanca.action;
 
-public class SettlementCompleteAction {
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SettlementCompleteAction extends ActionSupport implements SessionAware{
+
+private String id;
+private
+
+
+
 
 }
