ede15c43a809543b4be0886bdac15fed194118a5
RFedNo1 <patrick-rafter@live.jp>
2018-11-07 16:45:58 +0900

    ishii committed

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -8,16 +8,32 @@
 <title>ログイン</title>
 </head>
 <body>
+	<s:if test="!loginIdErrorMessageList.isEmpty()">
+		<s:iterator value="loginIdErrorMessageList">
+			<s:property/>
+		</s:iterator>
+	</s:if>
+	<s:if test="!passwordErrorMessageList.isEmpty()">
+		<s:iterator value="passwordErrorMessageList">
+			<s:property/>
+		</s:iterator>
+	</s:if>
 	<s:form action="LoginAction">
-	<s:if test="#session.savedLoginId==true">
-		<s:textfield name="loginId" value="%{session.loginId}"/>
+	<s:if test="#session.isSavedLoginId">
+		<s:textfield name="loginId" value="#session.loginId"/>
 	</s:if>
 	<s:else>
 		<s:textfield name="loginId"/>
 	</s:else>
 		<s:password name="password"/>
-		<s:checkbox name="savedLoginId"/>
+		<s:checkbox name="isSavedLoginId"/>
 	<s:submit value="ログイン"/>
+	</s:form>
+	<s:form action="CreateUserAction">
+		<s:submit value="新規登録"/>
+	</s:form>
+	<s:form action="ResetPasswordAction">
+		<s:submit value="パスワード再設定"/>
 	</s:form>
 </body>
 </html>
--- src/com/internousdev/casablanca/action/LoginAction.java
+++ src/com/internousdev/casablanca/action/LoginAction.java
@@ -7,30 +7,49 @@ import org.apache.struts2.interceptor.SessionAware;
 import com.internousdev.casablanca.dao.DestinationInfoDAO;
 import com.internousdev.casablanca.dao.UserInfoDAO;
 import com.internousdev.casablanca.dto.DestinationInfoDTO;
+import com.internousdev.casablanca.util.InputChecker;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport implements SessionAware {
 	private String loginId;
 	private String password;
-	private boolean savedLoginId = false;
+	private boolean isSavedLoginId = false;
 	private List<DestinationInfoDTO> destinationInfoDTOList;
+	private List<String> loginIdErrorMessageList;
+	private List<String> passwordErrorMessageList;
 	Map<String, Object> session;
 
 	public String execute() {
 		String result = ERROR;
-		session.put("savedLoginId", savedLoginId);
-		UserInfoDAO userInfoDAO = new UserInfoDAO();
-		boolean loginFlg = userInfoDAO.getUserInfo(loginId, password);
-		if (loginFlg) {
-			/* カートの決済ボタン経由でログイン画面にきたかどうか判定。以下、"経由してきた"場合の処理 */
-			if ((boolean)session.get("fromCartFlg")) {
-				/* 宛先情報をsettlementConfirm.jspで表示用に生成 */
-				DestinationInfoDAO destinationInfoDAO = new DestinationInfoDAO();
-				destinationInfoDTOList = destinationInfoDAO.getDestinationInfo(loginId);
-				result = "gotocart";
-			/* カート経由の場合 */
+
+		/* フォーム記憶チェック結果((true/falseで渡ってくる)をセッションへ格納 */
+		session.put("isSavedLoginId", isSavedLoginId);
+
+		/* 入力チェック */
+		InputChecker inputChecker = new InputChecker();
+		loginIdErrorMessageList = inputChecker.doCheck("ログインID", loginId, 1, 8, true, false, false, true, false, false, false);
+		passwordErrorMessageList = inputChecker.doCheck("ログインID", loginId, 1, 16, true, false, false, true, false, false, false);
+
+		/* 入力チェックがOKだった場合、ログイン処理へ。NGの場合は即ERRORをreturn */
+		if (loginIdErrorMessageList.size()==0 && passwordErrorMessageList.size()==0) {
+			UserInfoDAO userInfoDAO = new UserInfoDAO();
+			UserInfoDTO userInfoDTO = userInfoDAO.getUserInfo(loginId, password);
+			if (loginId.equals(userInfoDTO.getUserId()) && password.equals(userInfoDTO.getPassword())) {
+				session.put("loginId", userInfoDTO.getUserId());
+
+				/* ログインOKだった場合、DBカラム"logined"に1を立てる。そのloginedをsessionに取得 */
+				userInfoDTO = userInfoDAO.login(loginId, password);
+				session.put("logined", userInfoDTO.getLogined());
+
+				/* カートの決済ボタン経由(SettlementConfirmAction)でログイン画面にきたかどうか判定。以下、"経由してきた"場合の処理 */
+				if ((boolean)session.get("fromCartFlg")) {
+					/* 宛先情報をsettlementConfirm.jspで表示用に生成 */
+					DestinationInfoDAO destinationInfoDAO = new DestinationInfoDAO();
+					destinationInfoDTOList = destinationInfoDAO.getDestinationInfo(loginId);
+					result = "gotocart";
+				}
+				result = SUCCESS;
 			}
-			result = SUCCESS;
 		}
 		return result;
 	}
@@ -51,26 +70,23 @@ public class LoginAction extends ActionSupport implements SessionAware {
 		this.password = password;
 	}
 
-	public boolean isSavedLoginId() {
-		return savedLoginId;
+	public void setSavedLoginId(boolean isSavedLoginId) {
+		this.isSavedLoginId = isSavedLoginId;
 	}
 
-	public void setSavedLoginId(boolean savedLoginId) {
-		this.savedLoginId = savedLoginId;
+	public List<String> getLoginIdErrorMessageList() {
+		return loginIdErrorMessageList;
+	}
+
+	public List<String> getPasswordErrorMessageList() {
+		return passwordErrorMessageList;
 	}
 
 	public List<DestinationInfoDTO> getDestinationInfoDTOList() {
 		return destinationInfoDTOList;
 	}
 
-	public void setDestinationInfoDTOList(List<DestinationInfoDTO> destinationInfoDTOList) {
-		this.destinationInfoDTOList = destinationInfoDTOList;
-	}
-
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-
-
-
 }
