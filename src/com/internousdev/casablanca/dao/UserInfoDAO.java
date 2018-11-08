4a2626fd0cb0316a2514fa0a2718e476b72c6ee3
MasayaOuchi <mac.mac.jp5@icloud.com>
2018-11-08 11:26:49 +0900

    UserInfoDAOできました（たぶん）

--- src/com/internousdev/casablanca/dao/UserInfoDAO.java
+++ src/com/internousdev/casablanca/dao/UserInfoDAO.java
@@ -6,19 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.internousdev.casablanca.dto.UserInfoDTO;
-import com.internousdev.casablanca.util.DBConnctor
 import com.internousdev.casablanca.util.DBConnector;
 
 public class UserInfoDAO {
 	public int createUser(String familyName, String firstName, String familyNameKana, String firstNameKana, String sex, String email, String loginId, String password) {
-		DBConnector dbconnector = new DBConnector();
+		DBConnector dbConnector = new DBConnector();
 		Connection connection = dbConnector.getConnection();
 		int count =0;
 		String sql = "insert into user_info(user_id, password, familu_name, first_name, family_name_kana,"
 					+ " first_name_kana, sex, email, status, logine, rsgist_date, update_date)"
 					+ " values (?,?,?,?,?,?,?,?,?,?, now(), 0)";
 		try {
-			PreparedStatement = connection.preparedStatement(sql);
+			PreparedStatement preparedStatement= connection.prepareStatement(sql);
 			preparedStatement.setString(1, loginId);
 			preparedStatement.setString(2, password);
 			preparedStatement.setString(3, familyName);
@@ -27,55 +26,56 @@ public class UserInfoDAO {
 			preparedStatement.setString(6, firstNameKana);
 			preparedStatement.setString(7, sex);
 			preparedStatement.setString(8, email);
-			preparedStatement.setString(9, 0);
-			preparedStatement.setString(10, 1);
-			count = preparedStatement, executeUpdate();
+			preparedStatement.setInt(9, 0);
+			preparedStatement.setInt(10, 1);
+			count = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return count;
 	}
-	
+
 public boolean isExistsUserInfo(String loginId, String password) {
 	DBConnector dbConnector = new DBConnector();
 	Connection connection = dbConnector.getConnection();
 	boolean result = false;{
 	String sql = "select count(*) as count from user_info where user_id=? and password=?";
 	try {
-		PreparedStatement preparedStatement = connection.preparedStatement(sql);
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		preparedStatement.setString(1, loginId);
 		preparedStatement.setString(2, password);
 		ResultSet resultSet = preparedStatement.executeQuery();
-		while(resultSet.next() {
-			if (resultSet.getInt("count") <0)
-		}
+		while(resultSet.next()) {
+			if (resultSet.getInt("count") <0){
+				result = true;
+			}
 	}
-} cach(SQKException e) {
+} catch (SQLException e) {
 	e.printStackTrace();
-	
 }
 try {
-	connection.close();
-	
-}
-try	{
 	connection.close();
 }catch (SQLException e) {
 	e.printStackTrace();
 }
-return result;
+return result;} // ←????????????
 }
 
 public UserInfoDTO getUserInfo(String loginId, String password) {
 	DBConnector dbConnector = new DBConnector();
-	Connection connection =dbCOnnector.getConnection();
+	Connection connection =dbConnector.getConnection();
 	UserInfoDTO userInfoDTO = new UserInfoDTO();
-	String sql = "select * from user_info where user_id=? andpassword=?";
-	tru {
-		 PreparedStatement preparedStatement = connection.preparedStatement(sql);
+	String sql = "select * from user_info where user_id=? and password=?";
+	try {
+		 PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		 preparedStatement.setString(1, loginId);
-		 preparedStatement.setInt(String, password);
-		 ResuleSet resultSet = preparedStatemnt.executeQuery();
+		 preparedStatement.setString(2, password);
+		 ResultSet resultSet = preparedStatement.executeQuery();
 		 while(resultSet.next()) {
 			 userInfoDTO.setId(resultSet.getInt("id"));
 			 userInfoDTO.setUserId(resultSet.getString("user_id"));
@@ -85,14 +85,14 @@ public UserInfoDTO getUserInfo(String loginId, String password) {
 			 userInfoDTO.setFamilyNameKana(resultSet.getString("family_name_kana"));
 			 userInfoDTO.setFirstNameKana(resultSet.getString("first_name_kana"));
 			 userInfoDTO.setSex(resultSet.getInt("sex"));
-			 userInfoDTO.setEmail(rsultSet.getString("email"));
+			 userInfoDTO.setEmail(resultSet.getString("email"));
 			 userInfoDTO.setStatus(resultSet.getString("status"));
 			 userInfoDTO.setLogined(resultSet.getInt("status"));
 			 userInfoDTO.RegistDate(resultSet.getDate("regist_date"));
 			 userInfoDTO.setUpdateDate(resultSet.getDate("update_date"));
-		 }		 
+		 }
 	} catch (SQLException e) {
-		e.printStackTrace(););
+		e.printStackTrace();
 	}
 	try {
 		connection.close();
@@ -108,7 +108,7 @@ public UserInfoDTO getUserInfo(String loginId) {
 	UserInfoDTO userInfoDTO = new UserInfoDTO();
 	String sql = "select * from user_info where user_id=?";
 	try {
-		PreparedStatement preparedStatement = connection.preparedStatement(sql);
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		preparedStatement.setString(1, loginId);
 		ResultSet resultSet = preparedStatement.executeQuery();
 		while(resultSet.next()) {
@@ -118,15 +118,15 @@ public UserInfoDTO getUserInfo(String loginId) {
 			userInfoDTO.setFamilyName(resultSet.getString("family_name"));
 			userInfoDTO.setFirstName(resultSet.getString("firstName"));
 			userInfoDTO.setFamilyNameKana(resultSet.getString("family_name_kana"));
-			userIndoDTO.setFirstNameKane(resultSet.getString("first_name_kana"));
+			userInfoDTO.setFirstNameKane(resultSet.getString("first_name_kana"));
 			userInfoDTO.setSex(resultSet.getInt("sex"));
-			UserInfoDTO.setEmail(resultSet.getString("email"));
-			UserInfoDTO.setStatus(resultSet.getString("status"));
-			UserInfoDTO.setLogined(resultSet.getInt("logined"));
+			userInfoDTO.setEmail(resultSet.getString("email"));
+			userInfoDTO.setStatus(resultSet.getString("status"));
+			userInfoDTO.setLogined(resultSet.getInt("logined"));
 			userInfoDTO.setRegistDate(resultSet.getDate("regist_date"));
 			userInfoDTO.setUpdateDate(resultSet.getDate("update_date"));
 		}
-	}catch (SQLExeption e) {
+	}catch (SQLException e) {
 		e.printStackTrace();
 	}
 	try {
@@ -137,84 +137,17 @@ public UserInfoDTO getUserInfo(String loginId) {
 	return userInfoDTO;
 }
 
-public UserInfoDTO getUserInfo(String loginId, String password) {
-	DBConnector dbConnector =new DBConnector8	;
-	Connection connection = dbConnector.getConnection();
-	UserInfoDTO userInfoDTO = new UserInfoDTO();
-	String sql = "select * from user_info where user_id=?";
-	try{
-		PreparedStatement preparedStatement = connection.preparedStatement(sql);
-		preparedStatement.setString(1, loginId);
-		ResultSet resultSet = preparedStatement.executeQuery();
-		while(resultSet.next()) {
-			userInfoDTO.setId(resultSet.getInt("id"));
-			userInfoDTO.setUserID(resultSet.getString("user_id"));
-			userInfoDTO.setPassword(resultSet.getString("password"));
-			userInfoDTO.setFamilyName(resultSet.getString("family_name"));
-			userInfoDTO.setFirstName(resultSet.getString("first_name"));
-			userInfoDTO.setFamilyNameKana(resultSet.getString("family_name_kana"));
-			userInfoDTO.setFirstNameKana(resultSet.getString("first_name_kana"));
-			userInfoDTO.setSex(resultSet.getString("sex"));
-			userInfoDTO.setEmail(resultSet.getString("email"));
-			userInfoDTO.setStatus(resultSet.getString("status"));
-			userInfoDTO.setLogined(resultSet.getInt("status"));
-			userInfoDTO.setRegistDate(resultSet.getDate("regist_date"));
-			userInfoDTO.setUpdateDate(resultSet.getDate("update_date"));
-		}	
-	} catch (SQLExeption e) {
-		e.printStackTrace();
-	}
-	try {
-		connection.close();
-	} catch (SQLExeption e) {
-		e.printStackTrace() {
-			e.printStackTrace();
-		}
-		return userInfoDTO;
-	}
-	
-	public UserInfoDTO getUserInfo(String loginId) {
-		DBConnector dbConnector = new DBConnector();
-		Connection connection = dbConnector.getConnection();
-		UserINfoDTO userINfoDTO =new USerInfoDTO();
-		Stringsql = "select* ftom user_infO where User_id=?";
-		try {
-			PreparedStatement preparedStatement = connection.preparedStatement(dql);
-			preparedStatement.setString(1, loginId);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
-				userInfoDTO.setId(resultSet.getInt("id"));
-				userInfoDTO.setUserId(resultSet.getString("user_id"));
-				userInfoDTO.setPassword(resultSet.getString("password"));
-				userInfoDTO.setFamilyName(resultSet.getString("family_name"));
-				userINfoDTO.setFirstNameKana(resultSet.getString("first_name"))
-				userInfoDTO.setFamilyNameKana(resultSet.getString("family_name_kana"));
-				userInfoDTO.setFirstNameKana(resultSet.getString("first_name_Kana"));
-				userInfoDTO.setSex(resultSet.getInt("sex"));
-				userInfoDTO.setEmail(resulSet.getString("email"));
-				userInfoDTO.setStatus(resultSet.getString("status"));
-				userInfoDTO.setLogined(resulSet.getInt("logined"));
-				userinfoDTO.setRegistDate(resultSet.getDate("regist_date"));
-				userInfoDTO.setUpdateDate(resultSet.getDate("update_date"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return userInfoDTO;
-	}
-	
+
+
+
+
 	public int resetPassword(String loginId, String password) {
 		DBConnector dbConnector = new DBConnector();
-		Connection connecion = dbConnector.getConnection();
+		Connection connection = dbConnector.getConnection();
 		String sql = "update user_info set password=? where user_id=?";
 		int result = 0;
 		try {
-			PreparedStatement preparedStatement = connection.preparedStatement(sql);
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			preparedStatement.setString(1, password);
 			preparedStatement.setString(2, loginId);
 			result = preparedStatement.executeUpdate();
@@ -223,8 +156,64 @@ public UserInfoDTO getUserInfo(String loginId, String password) {
 		}
 		try {
 			connection.close();
-			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		return result;
+ 	}
+
+
+	public int login(String loginId, String password) {
+		DBConnector dbConnector =new DBConnector();
+		Connection connection = dbConnector.getConnection();
+		int result=0;
+		String sql = "update user_info set logined==1 where user_id=? and password=?";
+		try {
+			PreparedStatement preparedStatement =connection.prepareStatement(sql);
+			preparedStatement.setString(1, loginId);
+			preparedStatement.setString(2, password);
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
+
+	public int logout(String loginId) {
+		DBConnector dbConnector = new DBConnector();
+		Connection connection = dbConnector.getConnection();
+		int result=0;
+		String sql = "update user_info set logined=0 where user_id=?";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement .setString(1, loginId);
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
+
+
+
+	public String concealPassword(String password) {
+		int beginIndex = 0;
+		int endIndex = 0;
+		}
+	StringBuilder stringBuilder = new StringBuilder("****************");
+ing(beginIndex,en
+	String concealPassword = stringBuilder.replace(beginIndex, endIndex, password.substrdIndex).toString();
+	return concealPassword;
+
+		}
 }
