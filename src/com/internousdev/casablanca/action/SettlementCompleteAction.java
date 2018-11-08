50373aa95054dbee16437e19f8aa146674ae4376
kozueichikawa <wmwmwm1245@gmail.com>
2018-11-08 10:57:11 +0900

    commit

--- src/com/internousdev/casablanca/action/SettlementCompleteAction.java
+++ src/com/internousdev/casablanca/action/SettlementCompleteAction.java
@@ -1,15 +1,21 @@
 package com.internousdev.casablanca.action;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import org.apache.struts2.interceptor.SessionAware;
-
+import com.internousdev.casablanca.dao.DestinationInfoDAO;
+import com.internousdev.casablanca.dto.DestinationInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SettlementCompleteAction extends ActionSupport implements SessionAware{
+	List<DestinationInfoDTO> destinationInfoDTOList;
+	private Map<String,Object> session;
 
-private String id;
-private
+public String execute(){
+     String result = ERROR;
 
-
-
-
+	@SupporessWarnings("unchecked")
+	ArrayLIst<Purchas>
+}
 }
