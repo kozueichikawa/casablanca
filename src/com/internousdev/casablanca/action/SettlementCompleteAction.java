88549ad1a71cad035d2169d2797010cc271608ac
kozueichikawa <wmwmwm1245@gmail.com>
2018-11-09 14:28:03 +0900

    komi

--- src/com/internousdev/casablanca/action/SettlementCompleteAction.java
+++ src/com/internousdev/casablanca/action/SettlementCompleteAction.java
@@ -45,7 +45,7 @@ public class SettlementCompleteAction extends ActionSupport implements SessionAw
 	}
 	if(count > 0){
 		CartInfoDAO cartInfoDAO = new CartInfoDAO();
-		count = cartInfoDAO.deleteAll(String.valueOf(session.get("loginId")));
+		count = cartInfoDAO.delete(String.valueOf(session.get("loginId")));
 		if(count > 0){
 			List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();
 			cartInfoDtoList = cartInfoDAO.getCartInfoDtoList(String.valueOf(session.get("loginId")));
