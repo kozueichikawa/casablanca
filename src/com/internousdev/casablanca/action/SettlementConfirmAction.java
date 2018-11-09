e8fe6af9548f4339d12d0face1f6311a1c592925
RFedNo1 <patrick-rafter@live.jp>
2018-11-09 18:06:52 +0900

    ishii committed

--- src/com/internousdev/casablanca/action/SettlementConfirmAction.java
+++ src/com/internousdev/casablanca/action/SettlementConfirmAction.java
@@ -1,51 +1,194 @@
 package com.internousdev.casablanca.action;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.casablanca.dao.DestinationInfoDAO;
-import com.internousdev.casablanca.dao.MCategoryDAO;
-import com.internousdev.casablanca.dao.PurchaseHistoryInfoDAO;
 import com.internousdev.casablanca.dto.DestinationInfoDTO;
-import com.internousdev.casablanca.dto.MCategoryDTO;
 import com.internousdev.casablanca.dto.PurchaseHistoryInfoDTO;
+import com.internousdev.casablanca.util.CommonUtility;
 import com.opensymphony.xwork2.ActionSupport;
 
+public class SettlementConfirmAction extends ActionSupport implements SessionAware{
 
-public class SettlementConfirmAction extends ActionSupport implements SessionAware {
 	List<DestinationInfoDTO> destinationInfoDtoList;
-	private Map<String,Object> session;
-
+	private String categoryId;
+	private Collection<String> checkList;
+	private String productId;
+	private String productName;
+	private String productNameKana;
+	private String imageFilePath;
+	private String imageFileName;
+	private String price;
+	private String releaseCompany;
+	private String releaseDate;
+	private String productCount;
+	private String subtotal;
+	private Map<String, Object> session;
 	public String execute() {
 		String result = ERROR;
-		/* ログイン状態なら宛先選択画面へ宛先一覧をListで送り、未ログイン状態ならログイン画面へ */
-		if(Objects.equals(session.get("logined"), "1")){
-			result = SUCCESS;
-			DestinationInfoDAO destinationInfoDAO = new DestinationInfoDAO();
-			destinationInfoDtoList = destinationInfoDAO.getDestinationInfo(session.get("loginId").toString());
 
-			PurchaseHistoryInfoDAO purchaseHistoryInfoDAO = new PurchaseHistoryInfoDAO();
-			List<PurchaseHistoryInfoDTO> purchaseHistoryInfoDtoList = purchaseHistoryInfoDAO.getPurchaseHistoryList(session.get("loginId").toString());
-			session.put("purchaseHistoryInfoDtoList", purchaseHistoryInfoDtoList);
-		} else {
-			session.put("fromCart", true);
+		if(session.containsKey("loginId")) {
+			DestinationInfoDAO destinationInfoDAO = new DestinationInfoDAO();
+			destinationInfoDtoList = new ArrayList<>();
+			destinationInfoDtoList = destinationInfoDAO.getDestinationInfo(String.valueOf(session.get("loginId")));
+			Iterator<DestinationInfoDTO> iterator = destinationInfoDtoList.iterator();
+			if(!(iterator.hasNext())) {
+				destinationInfoDtoList = null;
+			}
 		}
-		if(!session.containsKey("mCategoryList")) {
-			MCategoryDAO mCategoryDAO=new MCategoryDAO();
-			List<MCategoryDTO> mCategoryDtoList= mCategoryDAO.getMCategoryList();
-			session.put("mCategoryDtoList", mCategoryDtoList);
+
+		List<PurchaseHistoryInfoDTO> purchaseHistoryInfoDtoList = new ArrayList<PurchaseHistoryInfoDTO>();
+
+		CommonUtility commonUtility = new CommonUtility();
+		String[] productIdList = commonUtility.parseArrayList(productId);
+		String[] productNameList = commonUtility.parseArrayList(productName);
+		String[] productNameKanaList = commonUtility.parseArrayList(productNameKana);
+		String[] imageFilePathList = commonUtility.parseArrayList(imageFilePath);
+		String[] imageFileNameList = commonUtility.parseArrayList(imageFileName);
+		String[] priceList = commonUtility.parseArrayList(price);
+		String[] releaseCompanyList = commonUtility.parseArrayList(releaseCompany);
+		String[] releaseDateList = commonUtility.parseArrayList(releaseDate);
+		String[] productCountList = commonUtility.parseArrayList(productCount);
+		String[] subtotalList = commonUtility.parseArrayList(subtotal);
+		for(int i=0;i<productIdList.length;i++) {
+			PurchaseHistoryInfoDTO purchaseHistoryInfoDTO = new PurchaseHistoryInfoDTO();
+			purchaseHistoryInfoDTO.setUserId(String.valueOf(session.get("loginId")));
+			purchaseHistoryInfoDTO.setProductId(Integer.parseInt(String.valueOf(productIdList[i])));
+			purchaseHistoryInfoDTO.setProductName(String.valueOf(productNameList[i]));
+			purchaseHistoryInfoDTO.setProductNameKana(String.valueOf(productNameKanaList[i]));
+			purchaseHistoryInfoDTO.setImageFilePath(String.valueOf(imageFilePathList[i]));
+			purchaseHistoryInfoDTO.setImageFileName(String.valueOf(imageFileNameList[i]));
+			purchaseHistoryInfoDTO.setPrice(Integer.parseInt(String.valueOf(priceList[i])));
+			purchaseHistoryInfoDTO.setReleaseCompany(String.valueOf(releaseCompanyList[i]));
+			try {
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+				purchaseHistoryInfoDTO.setReleaseDate(simpleDateFormat.parse(String.valueOf(releaseDateList[i])));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			purchaseHistoryInfoDTO.setProductCount(Integer.parseInt(String.valueOf(productCountList[i])));
+			purchaseHistoryInfoDTO.setSubtotal(Integer.parseInt(String.valueOf(subtotalList[i])));
+			purchaseHistoryInfoDtoList.add(purchaseHistoryInfoDTO);
+		}
+		session.put("purchaseHistoryInfoDtoList", purchaseHistoryInfoDtoList);
+
+		if(!session.containsKey("loginId")) {
+			result = ERROR;
+		}else {
+			result = SUCCESS;
 		}
 		return result;
 	}
 
-	public List<DestinationInfoDTO> getDestinationInfoDtoList() {
-		return destinationInfoDtoList;
+	public Collection<String> getCheckList() {
+		return checkList;
 	}
 
-@Override
+	public void setCheckList(Collection<String> checkList) {
+		this.checkList = checkList;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductNameKana() {
+		return productNameKana;
+	}
+
+	public void setProductNameKana(String productNameKana) {
+		this.productNameKana = productNameKana;
+	}
+
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getReleaseCompany() {
+		return releaseCompany;
+	}
+
+	public void setReleaseCompany(String releaseCompany) {
+		this.releaseCompany = releaseCompany;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getProductCount() {
+		return productCount;
+	}
+
+	public void setProductCount(String productCount) {
+		this.productCount = productCount;
+	}
+
+	public String getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(String subtotal) {
+		this.subtotal = subtotal;
+	}
+
 }
